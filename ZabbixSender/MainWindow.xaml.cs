c8c0cdfb5ed8e5c72a4ee497014978a3041b06c6
Alinhodeparis <diabagatebaly@gmail.com>
2023-05-24 16:58:15 +0700

    Add project files.

---
+++ ZabbixSender/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Timers;
+using ZabbixSender;
+using Newtonsoft.Json;
+
+
+namespace ZabbixSender
+{
+    
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Connection Zabbix 
+
+           /* var url = "home.vicdmitrienko.com/api_jsonrpc.php";
+            var request = HttpWebResponse;
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.ContentType = "application/json";*/
+
+
+            
+            var sender = new ZabbixSender("home.vicdmitrienko.com\", 10051");
+           
+            Timer time = new Timer(10000);  // timer tyo send the data every 10 s
+            timer.Elapsed += (sender, e) =>
+            {
+
+                var host = new ZabbixHost("ru.indorsoft.bali");
+                var item = new ZabbixItem("zabbix-trapper", "numeric");
+
+                var result = sender.send(item, host);
+                if (result.succes)
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to send item:");
+                    Console.WriteLine("result.ErrorMessage");
+                }
+
+            };
+            sender.Start();
+
+            Console.ReadLine();           
+            
+
+        }
+
+        private static void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class timer
+    {
+        internal static Action<object, object> Elapsed;
+    }
+}
+
+
